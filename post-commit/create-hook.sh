9d2aa6257925ccba3b447cc0a3e3e975ca5ebd70
Student User <student@workstation.lab.example.com>
2021-01-10 09:09:19 -0500

    new files

---
+++ post-commit/create-hook.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /usr/local/etc/ocp4.config
+"\"curl -s -S -i -X POST http://builds-for-managers-${RHT_OCP4_DEV_USER}-post-commit.${RHT_OCP4_WILDCARD_DOMAIN}/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
