982704c4cbf517dfffbbdaded2de23490032ff3e
Student User <student@workstation.lab.example.com>
2021-01-10 09:58:03 -0500

    file

--- post-commit/create-hook.py
+++ post-commit/create-hook.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 
-source /usr/local/etc/ocp4.config
+#execfile("/usr/local/etc/ocp4.config")
+print {RHT_OCP4_DEV_USER}
 "\"curl -s -S -i -X POST http://builds-for-managers-${RHT_OCP4_DEV_USER}-post-commit.${RHT_OCP4_WILDCARD_DOMAIN}/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
--- post-commit/create-hook.sh
+++ post-commit/create-hook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /usr/local/etc/ocp4.config
-"\"curl -s -S -i -X POST http://builds-for-managers-${RHT_OCP4_DEV_USER}-post-commit.${RHT_OCP4_WILDCARD_DOMAIN}/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
+#source /usr/local/etc/ocp4.config
+"\"curl -s -S -i -X POST http://builds-for-managers-bouv-ch-ibm-com-post-commit.apps.eu45.prod.nextcle.com/api/builds -f -d 'developer=\${DEVELOPER}&git=\${OPENSHIFT_BUILD_SOURCE}&project=\${OPENSHIFT_BUILD_NAMESPACE}'\""
